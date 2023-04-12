9e6559c0ccd01620ebe8f8bd76c395188cfc4ef7
Pokdee <loselife61@gmail.com>
2023-04-12 23:12:03 +0700

    Implement player Changing system

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const icon = document.querySelector(".icon");
 const player1 = document.querySelector(".player_1");
 const player2 = document.querySelector(".player_2");
 const select = document.querySelector(".select_icon");
-
 const gameboard = [...Array(9)].map((x) => " ");
+let target;
+let checker = true;
 
 const game = () => {
   //color change
@@ -18,73 +19,71 @@ const game = () => {
 
   //Game Start
 
-  const gameStart = function (type) {
-    colorCont("green", "red");
-    clicker(type);
-  };
-
-  //Click displayer
-  const clicker = function (type) {
-    board.addEventListener("click", (e) => {
-      console.log(type);
-
-      if (e.target.closest(".cell")) {
-        select.style.display = "none";
-
-        const cellNum = e.target.getAttribute("id");
-        const cell = e.target.children[0];
-        console.log(cell, cellNum);
-
-        //if X
-        if (type === "X") {
-          // if (!cell.classList.contains("cellInX")) {
-          cell.classList.add("cellInX");
-
-          gameboard[cellNum] = "X";
-          console.log(gameboard);
-          // }
-        }
-
-        //if O
-        if (type === "O") {
-          // if (!cell.classList.contains("cellInO")) {
-          cell.classList.add("cellInO");
-
-          gameboard[cellNum] = "O";
-          console.log(gameboard);
-          // }
-        }
-      }
-      switchplayer(2);
-    });
+  const gameStart = function (event) {
+    target = event.target;
+    if (checker) {
+      const logo = icon.value;
+      clicker(logo);
+    }
+    if (!checker) {
+      const logo = icon.value === "X" ? "O" : "X";
+      clicker(logo);
+    }
+    checker = !checker;
   };
 
   //Switch player
 
-  const switchplayer = function (player) {
+  const playerChange = function (player) {
     const playerIcon = icon.value;
-    console.log("switch");
+    const comIcon = playerIcon === "X" ? "O" : "X";
 
-    //player 1
     if (player === 1) {
-      colorCont("green", "red");
+      clicker(playerIcon);
+      checker = !checker;
+      console.log(checker);
     }
-
-    //player 2
     if (player === 2) {
-      colorCont("red", "green");
-      // const iconType = icon.value === "X" ? "O" : "X";
-      clicker("O");
-      //
+      clicker(comIcon);
+      checker = !checker;
     }
   };
 
-  return { clicker, gameStart, switchplayer, colorCont };
+  //Click displayer
+  const clicker = function (icon) {
+    if (target.closest(".cell")) {
+      select.style.display = "none";
+
+      const cellNum = target.getAttribute("id");
+      const cell = target.children[0];
+      if (cell.classList.contains("cellInX" || "cellInO")) {
+        return;
+      } else {
+        //if X
+        if (icon === "X") {
+          cell.classList.add("cellInX");
+
+          gameboard[cellNum] = "X";
+          // console.log(gameboard);
+        }
+
+        //if O
+        if (icon === "O") {
+          cell.classList.add("cellInO");
+
+          gameboard[cellNum] = "O";
+          // console.log(gameboard);
+        }
+      }
+    }
+  };
+
+  return { clicker, gameStart, playerChange, colorCont };
 };
 
 const Board = game();
 
-Board.gameStart(icon.value);
+board.addEventListener("click", (e) => Board.gameStart(e));
 
 //Display Gameboard array result
 //  const display = function () {
